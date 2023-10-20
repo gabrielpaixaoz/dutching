f01e8c32bab051e34daa36bca37732ed700ef95b
gabrielpaixaoz <gabrielpaix54@gmail.com>
2023-10-20 10:23:47 -0300

    teste

--- script2.js
+++ script2.js
@@ -1,6 +1,30 @@
-function dutching(stake, prob){
-    var total_prob = 0;
+function dutching(totalbet,prob){
+    
+    var bet = dutching(totalbet,prob);
+   var  totalprob  = 0;
+    
+    for(var i = 0; i < prob.length; i++){
+        totalprob += 1 / prob[i];
+    }
+
+    var bets = [];
+    
+    for(var i = 0; i < prob.length; i++){
+       var bet = totalbet / (prob[i] * totalprob);
+        bets.push(bet.toFixed(2))
+       
+    }
+    var output = document.querySelector('section#output');
+    output.innerHTML = `${bets}`
+    
     
 }
+var totalbet= Number(document.querySelector('input#investimento').value)
+var prob = Number(document.querySelector('input#number1').value)
+
+
+
+
+
 
 
