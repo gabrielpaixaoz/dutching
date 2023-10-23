f313cb8830e8a470926ddc235be63c9b8251ead8
gabrielpaixaoz <gabrielpaix54@gmail.com>
2023-10-23 18:36:02 -0300

    2

--- script2.js
+++ script2.js
@@ -1,7 +1,14 @@
+function calcular(){
+
+
 function dutching(totalbet,prob){
-    
-    var bet = dutching(totalbet,prob);
-   var  totalprob  = 0;
+    var totalbet = Number(document.querySelector('input#investimento').value)
+    var probInputs = document.querySelector('.numbers')
+    var prob = [];
+    probInputs.forEach(function(input) {
+        prob.push(Number(input.value)); 
+    });
+    var  totalprob  = 0;
     
     for(var i = 0; i < prob.length; i++){
         totalprob += 1 / prob[i];
@@ -14,17 +21,50 @@ function dutching(totalbet,prob){
         bets.push(bet.toFixed(2))
        
     }
-    var output = document.querySelector('section#output');
-    output.innerHTML = `${bets}`
+    return bets;
+   
     
     
 }
-var totalbet= Number(document.querySelector('input#investimento').value)
-var prob = Number(document.querySelector('input#number1').value)
 
 
 
 
 
+var bets = dutching(totalbet,prob);
+var output = document.querySelector('section#output')
+output.innerHTML = `Resultado: ${bets.join(', ')}`;
+}
 
 
+
+
+/*
+function calcular() {
+    function dutching(prob) {
+        var totalbet = Number(document.querySelector('input#investimento').value);
+        var probInputs = document.querySelectorAll('.numbers');
+        var prob = [];
+        probInputs.forEach(function(input) {
+            prob.push(Number(input.value));
+        });
+        var totalprob = 0;
+        
+        for (var i = 0; i < prob.length; i++) {
+            totalprob += 1 / prob[i];
+        }
+
+        var bets = [];
+        
+        for (var i = 0; i < prob.length; i++) {
+            var bet = totalbet / (prob[i] * totalprob);
+            bets.push(bet.toFixed(2));
+        }
+        return bets;
+    }
+
+    var bets = dutching();
+    var output = document.querySelector('section#output');
+    output.innerHTML = `Resultados: ${bets.join(', ')}`;
+}
+*/
