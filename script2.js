12b5068e530791b87b10987447ff9a0e86557c14
gabrielpaixaoz <gabrielpaix54@gmail.com>
2023-10-24 12:40:28 -0300

    2

--- script2.js
+++ script2.js
@@ -1,31 +1,29 @@
 function calcular(){
+    function dutching(prob){
+        var totalbet = Number(document.querySelector('input#investimento').value)
+        var probInputs = document.querySelectorAll('.numbers')
+        var prob = [];
+        probInputs.forEach(function(input) {
+            prob.push(Number(input.value));
+        });
+        var  totalprob  = 0;
+        
+        for(var i = 0; i < prob.length; i++){
+            totalprob += 1 / prob[i];
+        }
 
-
-function dutching(prob){
-    var totalbet = Number(document.querySelector('input#investimento').value)
-    var probInputs = document.querySelector('.numbers')
-    var prob = [];
-    probInputs.forEach(function(input) {
-        prob.push(Number(input.value)); 
-    });
-    var  totalprob  = 0;
-    
-    for(var i = 0; i < prob.length; i++){
-        totalprob += 1 / prob[i];
+        var bets = [];
+        
+        for(var i = 0; i < prob.length; i++){
+        var bet = totalbet / (prob[i] * totalprob);
+            bets.push(bet.toFixed(2))
+        }
+        return bets;
     }
 
-    var bets = [];
-    
-    for(var i = 0; i < prob.length; i++){
-       var bet = totalbet / (prob[i] * totalprob);
-        bets.push(bet.toFixed(2))
-    }
-    return bets;
-}
-
-var bets = dutching();
-var output = document.querySelector('section#output')
-output.innerHTML = `Resultado: ${bets.join(', ')}`;
+    var bets = dutching();
+    var output = document.querySelector('section#output')
+    output.innerHTML = `Resultado: ${bets.join(', ')}`;
 }
 
 
