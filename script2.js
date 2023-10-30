0e35aeb8a600364d43c2685416258e4207acb8ab
gabrielpaixaoz <gabrielpaix54@gmail.com>
2023-10-30 07:18:01 -0300

    311

--- script2.js
+++ script2.js
@@ -1,4 +1,20 @@
+
 function calcular() {
+    function formatarCampoInput(input) {
+        var valor = input.value.replace(/[^\d,]/g, ''); // Remove caracteres não numéricos, exceto vírgula
+        valor = valor.replace(',', '.'); // Substitui vírgula por ponto como separador decimal
+        if (valor.length >= 3) {
+            input.value = valor.slice(0, -2) + '.' + valor.slice(-2); // Adiciona o ponto decimal
+        } 
+    }
+    // NÃO ESQUECER DE REFAZER O CODIGO POR CAUSA DO CHATGPT
+    // Adiciona o evento input aos campos de entrada com type="number"
+    var probInputs = document.querySelectorAll('input[type="number"]');
+    probInputs.forEach(function(input) {
+        input.addEventListener('input', function() {
+            formatarCampoInput(input);
+        });
+    });
     function dutching() {
         var totalbet = Number(document.querySelector('input#investimento').value);
         var probInputs = document.querySelectorAll('input[type="number"]');
@@ -10,8 +26,6 @@ function calcular() {
                 prob.push(numero);
             }
         });
-
-       
 
         var totalprob = prob.reduce(function(acc, curr) {
             return acc + 1 / curr;
