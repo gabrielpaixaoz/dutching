0645916a9ba1a8a5900a962eafc1c5e0ac27ea05
gabrielpaixaoz <gabrielpaix54@gmail.com>
2023-11-01 15:51:17 -0300

    878

--- script2.js
+++ script2.js
@@ -39,22 +39,80 @@ function calcular() {
             return bet.toFixed(2);
         });
        
-           
+        
         
         return bets
 
     }
+    function retorno() {
+        var totalbet = Number(document.querySelector('input#investimento').value);
+        var probInputs = document.querySelectorAll('input[type="number"]');
+        var prob = [];
+    
+        probInputs.forEach(function(input) {
+            var numero = parseFloat(input.value) || 0;
+            if (numero !== 0) {
+                prob.push(numero);
+            }
+        });
+    
+        var totalprob = prob.reduce(function(acc, curr) {
+            return acc + 1 / curr;
+        }, 0);
+    
+        var bets = prob.map(function(probabilidade) {
+            var bet = totalbet / (probabilidade * totalprob);
+            return bet;
+        });
+    
+        // Multiplicar cada item de bets pelo correspondente item em probInputs
+        bets = bets.map(function(bet, index) {
+            return (bet * prob[index]).toFixed(2);
+        });
+    
+        return bets;
+    } 
     
    
-
-
+    var re = retorno()
     
     var bets = dutching();
     
         var output = document.querySelector('section#output');
-        output.innerHTML = `Resultado: ${bets.join(', ')} `;
+        output.innerHTML = `Resultado: ${bets.join(', ')} ${re}`;
       
 
         
     
 }
+
+
+/*function dutching() {
+    var totalbet = Number(document.querySelector('input#investimento').value);
+    var probInputs = document.querySelectorAll('input[type="number"]');
+    var prob = [];
+
+    probInputs.forEach(function(input) {
+        var numero = parseFloat(input.value) || 0;
+        if (numero !== 0) {
+            prob.push(numero);
+        }
+    });
+
+    var totalprob = prob.reduce(function(acc, curr) {
+        return acc + 1 / curr;
+    }, 0);
+
+    var bets = prob.map(function(probabilidade) {
+        var bet = totalbet / (probabilidade * totalprob);
+        return bet;
+    });
+
+    // Multiplicar cada item de bets pelo correspondente item em probInputs
+    bets = bets.map(function(bet, index) {
+        return (bet * prob[index]).toFixed(2);
+    });
+
+    return bets;
+} 
+*/
