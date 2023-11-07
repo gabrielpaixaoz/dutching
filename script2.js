1658e4bc1d1a15d1cbc3c864f02144d94d0299bf
gabrielpaixaoz <gabrielpaix54@gmail.com>
2023-11-07 12:12:28 -0300

    design

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@
     }
     
     // Adiciona o evento input aos campos de entrada com type="number"
-    var probInputs = document.querySelectorAll('input[type="number"]');
+    var probInputs = document.querySelectorAll('input[class="numbers"]');
     probInputs.forEach(function(input) {
         input.addEventListener('input', function() {
             formatarCampoInput(input);
@@ -16,7 +16,7 @@
     });
     function dutching() {
         var totalbet = Number(document.querySelector('input#investimento').value);
-        var probInputs = document.querySelectorAll('input[type="number"]');
+        var probInputs = document.querySelectorAll('input[class="numbers"]');
         var prob = [];
         
 
@@ -35,7 +35,7 @@
 
         var bets = prob.map(function(probabilidade) {
             var bet = totalbet / (probabilidade * totalprob);
-            return bet.toFixed(2);
+            return bet.toFixed(2)
         });
        
         
@@ -45,7 +45,7 @@
     }
     function retorno() {
         var totalbet = Number(document.querySelector('input#investimento').value);
-        var probInputs = document.querySelectorAll('input[type="number"]');
+        var probInputs = document.querySelectorAll('input[class="numbers"]');
         var prob = [];
     
         probInputs.forEach(function(input) {
@@ -83,10 +83,15 @@
     } 
     
     var totalbet = Number(document.querySelector('input#investimento').value)
+
     var span = document.querySelector('span#lucro')
+
     var re = retorno()
+
     var bets = dutching();
+
     var lucro = re[0] - totalbet
+
     var lucrofixed = lucro.toFixed(2)
   
     
@@ -101,17 +106,56 @@
    
 
         var output_lucro = document.querySelector('span#lucro')
+
         var output_retorno = document.querySelector('span#retorno')
-        var output = document.querySelector('span#output');
+
+        
+        
         var reload = document.querySelector('button#reload')
-        output.innerHTML = `<p>${bets.join('<br>')}<p>`;
+        
+        
         output_retorno.innerHTML = `${re[0]}`
+        
         output_lucro.innerHTML = `${lucrofixed}`
+        
         reload.addEventListener("click",function(){
             location.reload();
         })
 
-    
+        
+        var voltas = document.querySelectorAll(".voltas")
+
+        voltas.forEach(function(volta){
+            volta.innerHTML = `${re[0]}`
+        }
+        )
+        
+        var output1 = document.querySelector('span#output1');
+        output1.innerHTML = `${bets[0]}`;
+        var output2 = document.querySelector('span#output2')
+        output2.innerHTML = `${bets[1]}`
+        var output3 = document.querySelector('span#output3')
+        output3.innerHTML = `${bets[2]}`
+        var output4 = document.querySelector('span#output4')
+        output4.innerHTML = `${bets[3]}`
+        var output5 = document.querySelector('span#output5')
+        output5.innerHTML = `${bets[4]}`
+        var output6 = document.querySelector('span#output6')
+        output6.innerHTML = `${bets[5]}`
+        var output7 = document.querySelector('span#output7')
+        output7.innerHTML = `${bets[6]}`
+        var output8 = document.querySelector('span#output8')
+        output8.innerHTML = `${bets[7]}`
+        var output9 = document.querySelector('span#output9')
+        output9.innerHTML = `${bets[8]}`
+        var output10 = document.querySelector('span#output10')
+        output10.innerHTML = `${bets[9]}`
+        var output11 = document.querySelector('span#output11')
+        output11.innerHTML = `${bets[10]}`
+        var output12 = document.querySelector('span#output12')
+        output12.innerHTML = `${bets[11]}`
+
+        
 }
 
 
