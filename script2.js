249af0a97c54e2aa4ec66887321c405c488f1d37
gabrielpaixaoz <gabrielpaix54@gmail.com>
2023-11-09 11:31:17 -0300

    de

--- script2.js
+++ script2.js
@@ -37,7 +37,13 @@
             var bet = totalbet / (probabilidade * totalprob);
             return bet.toFixed(2)
         });
-       
+
+        
+
+        
+
+        
+        
         
         
         return bets
@@ -47,7 +53,7 @@
         var totalbet = Number(document.querySelector('input#investimento').value);
         var probInputs = document.querySelectorAll('input[class="numbers"]');
         var prob = [];
-    
+        
         probInputs.forEach(function(input) {
             var numero = parseFloat(input.value) || 0;
             if (numero !== 0) {
@@ -82,6 +88,8 @@
         return bets;
     } 
     
+   
+
     var totalbet = Number(document.querySelector('input#investimento').value)
 
     var span = document.querySelector('span#lucro')
@@ -93,7 +101,9 @@
     var lucro = re[0] - totalbet
 
     var lucrofixed = lucro.toFixed(2)
-  
+    
+    
+
     
     if(lucro <= 0){
         span.style.backgroundColor = "#A85954";
@@ -102,7 +112,12 @@
         span.style.backgroundColor = "#90A955";
     }
     
-    
+    for(let z = 0; z < re.length; z++){
+        if(re[z] === 0){
+            re[z] = ''
+        }
+        
+    }
    
 
         var output_lucro = document.querySelector('span#lucro')
@@ -123,68 +138,46 @@
         })
 
         
-        var voltas = document.querySelectorAll(".voltas")
-
-        voltas.forEach(function(volta){
-            volta.innerHTML = `${re[0]}`
-        }
-        )
-        
+         
         var output1 = document.querySelector('span#output1');
+        var volta1 = document.querySelector('span#volta1')
         output1.innerHTML = `${bets[0]}`;
+        volta1.innerHTML = `${re[0]}`
+        
         var output2 = document.querySelector('span#output2')
+        var volta2 = document.querySelector('span#volta2')
         output2.innerHTML = `${bets[1]}`
+        volta2.innerHTML = `${re[1]}`
+
         var output3 = document.querySelector('span#output3')
         output3.innerHTML = `${bets[2]}`
+
         var output4 = document.querySelector('span#output4')
         output4.innerHTML = `${bets[3]}`
+
         var output5 = document.querySelector('span#output5')
         output5.innerHTML = `${bets[4]}`
+
         var output6 = document.querySelector('span#output6')
         output6.innerHTML = `${bets[5]}`
+        
         var output7 = document.querySelector('span#output7')
         output7.innerHTML = `${bets[6]}`
+
         var output8 = document.querySelector('span#output8')
         output8.innerHTML = `${bets[7]}`
+
         var output9 = document.querySelector('span#output9')
         output9.innerHTML = `${bets[8]}`
+
         var output10 = document.querySelector('span#output10')
         output10.innerHTML = `${bets[9]}`
+
         var output11 = document.querySelector('span#output11')
         output11.innerHTML = `${bets[10]}`
+
         var output12 = document.querySelector('span#output12')
         output12.innerHTML = `${bets[11]}`
 
         
 }
-
-
-/*function dutching() {
-    var totalbet = Number(document.querySelector('input#investimento').value);
-    var probInputs = document.querySelectorAll('input[type="number"]');
-    var prob = [];
-
-    probInputs.forEach(function(input) {
-        var numero = parseFloat(input.value) || 0;
-        if (numero !== 0) {
-            prob.push(numero);
-        }
-    });
-
-    var totalprob = prob.reduce(function(acc, curr) {
-        return acc + 1 / curr;
-    }, 0);
-
-    var bets = prob.map(function(probabilidade) {
-        var bet = totalbet / (probabilidade * totalprob);
-        return bet;
-    });
-
-    // Multiplicar cada item de bets pelo correspondente item em probInputs
-    bets = bets.map(function(bet, index) {
-        return (bet * prob[index]).toFixed(2);
-    });
-
-    return bets;
-} 
-*/
